bc730c4ce04f2d044cc1e633fd778da780ae7ae1
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2023-02-22 14:51:37 +0300

    next step

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,7 +20,7 @@ export const Contact = (props) => {
     e.preventDefault();
     console.log(name, email, message);
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .sendForm("service_tr594im", "template_ha3gxkw", e.target, "DYmbEcXFm53bghbjZ")
       .then(
         (result) => {
           console.log(result.text);
